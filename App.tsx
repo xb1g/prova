0a17371960100288e0da9ab67ab3de4a9a16604e
Bunyasit Fang <wachaa1319@gmail.com>
2026-02-13 01:30:23 -0800

    app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { useFonts, Orbit_400Regular } from '@expo-google-fonts/orbit';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Orbit_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
+      <Text style={{ fontFamily: 'Orbit_400Regular', fontSize: 24 }}>
+        Orbit Font Installed!
+      </Text>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
